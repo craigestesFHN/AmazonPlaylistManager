0eeb87fb6dbd458a7dbbe7e26d3d2317c7ceb634
craigestesFHN <craig.estes@fhnfinancial.com>
2026-07-30 07:18:04 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ app.MapPost("/api/context", () => Results.Ok(new {
     timeOfDay = "Evening",
     mood = "Relaxed Acoustic"
 }));
-
+// 2. POST /api/music/search (Mocked catalog search)
+app.MapGet("/api/music/all", () => (Results.Ok);
 // 2. POST /api/music/search (Mocked catalog search)
 app.MapPost("/api/music/search", (SearchRequest req) => Results.Ok(new {
     resolvedTracks = req.Tracks.Select(t => new {
